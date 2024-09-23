272cf62cd810f68268f71e5c5a1439a7028e2c56
Enderiose <286115@qq.com>
2024-09-23 12:16:38 +0800

    Update dirmonitor

--- plugins/dirmonitor/__init__.py
+++ plugins/dirmonitor/__init__.py
@@ -60,11 +60,11 @@ class DirMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "directory.png"
     # 插件版本
-    plugin_version = "2.4"
+    plugin_version = "1.0"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "Enderiose"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/Enderiose"
     # 插件配置项ID前缀
     plugin_config_prefix = "dirmonitor_"
     # 加载顺序
